81bd0f69dbe49e8202018b9d38859dccb3812911
KhalilurRahmanKhan <khanjahid373@gmail.com>
2021-08-23 20:41:52 +0600

    responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,16 +7,23 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <div className="footer">
-           
-            <a href="https://www.facebook.com/KRKJahid"><FacebookIcon/></a>
+           <div className="hidden">
+           <a href="https://www.facebook.com/KRKJahid"><FacebookIcon/></a>
 
-            <a href="https://www.linkedin.com/in/khalilur-rahman-khan-034a81201/"><LinkedInIcon/></a>
+        <a href="https://www.linkedin.com/in/khalilur-rahman-khan-034a81201/"><LinkedInIcon/></a>
 
-            <a href="https://github.com/KhalilurRahmanKhan"><GitHubIcon/></a>
-
-            <p>Copyright@KRK</p>
+        <a href="https://github.com/KhalilurRahmanKhan"><GitHubIcon/></a>
+           </div>
+          
+           <p>Email: Khanjahid373@gmail.com</p>
+            <p>Phone: 01850814547</p>
+            
+            <div className="last">
+            <i>Copyright@KRK</i><br />
             <i>All Rights Reserved</i>
           
+            </div>
+            
         </div>
     );
 };
