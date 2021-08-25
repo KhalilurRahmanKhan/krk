fc43354f0c496b9a515020faa4fed8cdd184eaf2
KhalilurRahmanKhan <khanjahid373@gmail.com>
2021-08-25 21:06:04 +0600

    responsive

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,7 @@ import Myself from "../../assets/myself.png";
 const About = () => {
     return (
         <div className="about" id="about">
-            <h3>About Me</h3>
+           
         {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,160L0,288L180,288L180,128L360,128L360,96L540,96L540,160L720,160L720,32L900,32L900,192L1080,192L1080,32L1260,32L1260,224L1440,224L1440,0L1260,0L1260,0L1080,0L1080,0L900,0L900,0L720,0L720,0L540,0L540,0L360,0L360,0L180,0L180,0L0,0L0,0Z"></path></svg> */}
 
             
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,27 +1,20 @@
 import React from 'react';
 import './Sidebar.css';
-import HomeIcon from '@material-ui/icons/Home';
-import PersonIcon from '@material-ui/icons/Person';
-import CastForEducationIcon from '@material-ui/icons/CastForEducation';
-import TimelineIcon from '@material-ui/icons/Timeline';
-import WorkIcon from '@material-ui/icons/Work';
-import RoomServiceIcon from '@material-ui/icons/RoomService';
-import SpeakerNotesIcon from '@material-ui/icons/SpeakerNotes';
-import ContactsIcon from '@material-ui/icons/Contacts';
+
 
 const Sidebar = ({menu ,setMenu}) => {
    
     return (
         <div className={"sidebar " + (menu && 'active')}>
             <ul>
-               <li onClick={() => setMenu(!menu)}><a href="#home" > <HomeIcon/> </a></li>
-                <li onClick={() => setMenu(!menu)}><a href="#about" > <PersonIcon/> </a></li>
-                <li onClick={() => setMenu(!menu)}><a href="#skills"> <CastForEducationIcon/> </a></li>
-                <li onClick={() => setMenu(!menu)}><a href="#resume"> <TimelineIcon/> </a></li>
-                <li onClick={() => setMenu(!menu)}><a href="#portfolio"> <WorkIcon/> </a></li>
-                <li onClick={() => setMenu(!menu)}><a href="#services"> <RoomServiceIcon/> </a></li>
-                <li onClick={() => setMenu(!menu)}><a href="#testimonials"> <SpeakerNotesIcon/> </a></li>
-                <li onClick={() => setMenu(!menu)}><a href="#contact"> <ContactsIcon/> </a></li>
+               <li onClick={() => setMenu(!menu)}><a href="#home" > Home </a></li>
+                <li onClick={() => setMenu(!menu)}><a href="#about" > About </a></li>
+                <li onClick={() => setMenu(!menu)}><a href="#skills"> Skills </a></li>
+                <li onClick={() => setMenu(!menu)}><a href="#resume"> Resume </a></li>
+                <li onClick={() => setMenu(!menu)}><a href="#portfolio"> Portfolio </a></li>
+                <li onClick={() => setMenu(!menu)}><a href="#services"> Services </a></li>
+                <li onClick={() => setMenu(!menu)}><a href="#testimonials"> Testimonials </a></li>
+                <li onClick={() => setMenu(!menu)}><a href="#contact"> Contact </a></li>
             </ul>
         </div>
     );
